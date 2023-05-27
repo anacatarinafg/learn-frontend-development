b06cfee4df682a97c7280d2ed64642b5a1310051
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-05-27 10:58:36 +0100

    fixing layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Responsivetest />
+      <Test />
     </div>
   );
 }
--- src/components/responsivetest.jsx
+++ src/components/responsivetest.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { CSSRulePlugin } from 'gsap/all';
-import './responsivetest.css';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { CSSRulePlugin } from "gsap/all";
+import "./responsivetest.css";
 
 const Responsivetest = () => {
     const tl = useRef(gsap.timeline({ paused: true })).current;
@@ -9,74 +9,111 @@ const Responsivetest = () => {
     const path = useRef(null);
 
     useEffect(() => {
-        spanBefore.current = CSSRulePlugin.getRule('hamburger span:before');
-        gsap.set(spanBefore.current, { background: '#000' });
-        gsap.set('.navbar__menu', { visibility: 'hidden' });
+        spanBefore.current = CSSRulePlugin.getRule("hamburger span:before");
+        gsap.set(spanBefore.current, { background: "#000" });
+        gsap.set(".navbar__menu", { visibility: "hidden" });
     }, []);
 
     const revealMenu = () => {
         revealMenuItems();
 
-        const hamburger = document.getElementById('hamburger');
-        const toggleBtn = document.getElementById('toggle-btn');
+        const hamburger = document.getElementById("hamburger");
+        const toggleBtn = document.getElementById("toggle-btn");
 
         toggleBtn.onclick = function (e) {
-            hamburger.classList.toggle('active');
+            hamburger.classList.toggle("active");
             tl.reversed(!tl.reversed());
         };
     };
 
     const revealMenuItems = () => {
-        const start = 'M0 502S175 272 500 272s500 230 500 230V0H0Z';
-        const end = 'M0,1005S175,995,500,995s500, 5, 500, 5V0H0Z';
+        const start = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+        const end = "M0 1005S175 995 500 995s500 5 500 5V0H0Z";
 
-        const power2 = 'power2.inOut';
-        const power4 = 'power4.inOut';
+        const power2 = "power2.inOut";
+        const power4 = "power4.inOut";
 
-        tl.to('#hamburger', 1.25, {
-            marginTop: '-5px',
+        tl.to("#hamburger", 1.25, {
+            marginTop: "-5px",
             x: -40,
             y: 40,
             ease: power4,
         });
-        tl.to('#hamburger span', 1, {
-            background: '#e2e2dc',
-            ease: power2,
-        }, '<');
-        tl.to(spanBefore.current, 1, {
-            background: '#e2e2dc',
-            ease: power2,
-        }, '<');
-        tl.to('.navbar__button .navbar__buttonOutline', 1.25, {
-            x: -40,
-            y: 40,
-            width: '140px',
-            height: '140px',
-            border: '1px solid #e2e2dc',
-            ease: power4,
-        }, '<');
-        tl.to(path.current, 0.8, {
-            attr: {
-                d: start,
+        tl.to(
+            "#hamburger span",
+            1,
+            {
+                background: "#e2e2dc",
+                ease: power2,
             },
-            ease: 'power2.inOut', // Updated from Power2.easeIn
-        }, '<').to(path.current, 0.8, {
-            attr: {
-                d: end,
+            "<"
+        );
+        tl.to(
+            spanBefore.current,
+            1,
+            {
+                background: "#e2e2dc",
+                ease: power2,
             },
-            ease: 'power2.inOut', // Updated from Power2.easeIn
-        }, '-0.5');
+            "<"
+        );
+        tl.to(
+            ".navbar__button .navbar__buttonOutline",
+            1.25,
+            {
+                x: -40,
+                y: 40,
+                width: "140px",
+                height: "140px",
+                border: "1px solid #e2e2dc",
+                ease: power4,
+            },
+            "<"
+        );
+        tl.to(
+            path.current,
+            0.8,
+            {
+                opacity: 1, // Update the opacity to 1
+                attr: {
+                    d: start,
+                },
+                ease: "power2.inOut",
+            },
+            "<"
+        ).to(
+            path.current,
+            0.8,
+            {
+                opacity: 1, // Update the opacity to 1
+                attr: {
+                    d: end,
+                },
+                ease: "power2.inOut",
+            },
+            "-0.5"
+        );
 
-        tl.to('.navbar__menu', 1, {
-            visibility: 'visible',
-        }, '-=0.5');
-        tl.to('.menu__item > a', 1, {
-            top: 0,
-            ease: 'power3.out',
-            stagger: {
-                amount: 0.5,
+        tl.to(
+            ".navbar__menu",
+            1,
+            {
+                visibility: "visible",
             },
-        }, '-=1').reverse();
+            "-=0.5"
+        );
+        tl.to(
+            ".menu__item > a",
+            1,
+            {
+                top: 0,
+                ease: "power3.out",
+                stagger: {
+                    amount: 0.5,
+                },
+            },
+            "-=1"
+        ).reverse();
     };
 
     useEffect(() => {
@@ -96,15 +133,18 @@ const Responsivetest = () => {
 
             {/* Svg overlay */}
             <div>
-                <svg className="navbar__overlay">
-                    <path ref={path} d="M0 2S175 1 500 1s500 1 500 1V0H0Z"></path>
+                <svg className="navbar__overlay" viewBox="0 0 200 100">
+                    <path
+                        ref={path}
+                        d="M0 0 L100 0 L100 100 L0 100 Z"
+                        style={{ opacity: 0 }}
+                    ></path>
                 </svg>
             </div>
 
             {/* Menu items */}
             <div className="navbar__menu">
                 <div className="menu__primary">
-                    <h1 className="menu__title">Purity of noise</h1>
                     <div className="menu__container">
                         <div className="menu__wrapper">
                             <div className="menu__item">
