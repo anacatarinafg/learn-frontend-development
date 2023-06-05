63f4999df4009e82325bd51aec62e9c3c42542f3
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-06-05 15:17:28 +0100

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-
 import Navbar from "./Navbar/Navbar";
 import Hamburger from "./Hamburger/Hamburger";
 
-const Layout = ({ children }) => {
-    return (
-        <div className="layout">
-            <header>
-                <Navbar />
-                <Hamburger />
-            </header>
-            <main>{children}</main>
-        </div>
-    )
+const Layout = ({ children, showNavbar = true }) => {
+  return (
+    <div className="layout">
+      <header>
+        {showNavbar && <Navbar />}
+        <Hamburger />
+      </header>
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
